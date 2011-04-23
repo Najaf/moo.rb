781ccaff87380f1db9c7fca15610d84ea33526d2
Najaf Ali <ali.najaf@gmail.com>
2011-04-23 19:46:33 +0100

    made a start on the pack model

--- lib/moo/model/pack.rb
+++ lib/moo/model/pack.rb
@@ -1,7 +1,23 @@
 module Moo
   module Model
     class Pack
-      attr_accessor :cards, :sides
+      attr_accessor :cards, :sides, :product_code
+
+      PRODUCT_CODES = ['businesscard', 'minicard', 'postcard', 'holidaycard', 'sticker'].freeze
+
+      def initialize
+        @cards = []
+        @sides = []
+      end
+
+      def product_code=code
+        unless PRODUCT_CODES.include? code
+          raise(ArgumentError, 
+            "Invalid product code '#{code}', must be one of [#{PRODUCT_CODES.join(', ')}]"
+          )
+        end
+        @product_code = code
+      end
     end
   end
 end
--- spec/model/pack_spec.rb
+++ spec/model/pack_spec.rb
@@ -1,4 +1,38 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+include Moo::Model
 
 describe Moo::Model::Pack do
+  def product_codes
+    ['businesscard', 'minicard', 'postcard', 'holidaycard', 'sticker']
+  end
+
+  describe 'initialize' do
+    it 'should initialize cards to empty array' do
+      Pack.new.cards.should == []
+    end
+
+    it 'should initialize sides to empty array' do
+      Pack.new.sides.should == []
+    end
+  end
+
+  describe 'product_code=' do
+    it "should set the product code" do
+      pack = Pack.new
+      pack.product_code = 'businesscard'
+      pack.product_code.should == 'businesscard'
+    end
+    it 'should complain on invalid product code' do
+      pack = Pack.new
+      expect { pack.product_code = 'slarty' }.should(
+        raise_error ArgumentError, "Invalid product code 'slarty', must be one of [#{product_codes.join(', ')}]"
+      )
+    end
+
+    it "should not complain on a valid product code" do
+      pack = Pack.new
+      expect {pack.product_code = 'businesscard'}.should_not raise_error ArgumentError
+    end
+
+  end
 end
