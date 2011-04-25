fc8229401ee0933606981d31bddb987c20c7fb24
Najaf Ali <ali.najaf@gmail.com>
2011-04-25 23:13:30 +0100

    added side data classes to main includes... probably need to think of a better strategy for loading classes...

--- lib/moo/model.rb
+++ lib/moo/model.rb
@@ -1,3 +1,12 @@
+# side data
+require 'moo/model/data'
+require 'moo/model/box_data'
+require 'moo/model/fixed_image_data'
+require 'moo/model/image_data'
+require 'moo/model/multi_line_text_data'
+require 'moo/model/text_data'
+
+# pack model
 require 'moo/model/pack'
 require 'moo/model/card'
 require 'moo/model/side'
