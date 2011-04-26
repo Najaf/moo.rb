0a01274d8f3493c552133748aae9d99d21bdee00
Najaf Ali <ali.najaf@gmail.com>
2011-04-26 10:20:27 +0100

    added some validation to colour

--- lib/moo/model/colour.rb
+++ lib/moo/model/colour.rb
@@ -2,6 +2,15 @@ module Moo
   module Model
     class Colour
       attr_accessor :type, :c, :m, :y, :k, :r, :g, :b
+
+      def type=new_type
+        unless ['cmyk','rgb','CMYK','RGB'].include? new_type
+          raise ArgumentError, 
+            "type must be either 'cmyk' or 'rgb', '#{new_type}' given"
+        end
+        @type = new_type.upcase
+      end
+
     end
   end
 end
--- spec/model/colour_spec.rb
+++ spec/model/colour_spec.rb
@@ -3,4 +3,22 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 include Moo::Model
 
 describe Moo::Model::Colour do
+  describe 'type=' do
+    it 'sets the type field' do
+      c = Colour.new
+      c.type = 'CMYK'
+      c.type.should == 'CMYK'
+    end
+    it 'upcases the type field' do
+      c = Colour.new
+      c.type = 'cmyk'
+      c.type.should == 'CMYK'
+    end
+    it "complains when you set a type that isn't 'cmyk' or 'rgb' (case insensitive)" do
+      c = Colour.new
+      expect { c.type = 'blah' }.should(
+        raise_error ArgumentError, "type must be either 'cmyk' or 'rgb', 'blah' given"
+      )
+    end
+  end
 end
