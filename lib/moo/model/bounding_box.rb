20748bcd7fd77b512b0a648a76a88a7b787debb8
Najaf Ali <ali.najaf@gmail.com>
2011-05-06 19:06:06 +0100

    added bounded box centre setter

--- lib/moo/model/bounding_box.rb
+++ lib/moo/model/bounding_box.rb
@@ -2,6 +2,18 @@ module Moo
   module Model
     class BoundingBox
       attr_accessor :centre, :width, :height, :angle
+
+      def initialize
+        @centre = [0,0]
+        @width, @height, @angle = 0, 0, 0
+      end
+
+      def centre=value
+        unless value.is_a? Array or value.is_a? Hash
+          raise ArgumentError, "value must be a hash or an array, #{value.class} given"
+        end
+        @centre = value
+      end
     end
   end
 end
--- spec/model/bounding_box_spec.rb
+++ spec/model/bounding_box_spec.rb
@@ -3,4 +3,39 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 include Moo::Model
 
 describe Moo::Model::BoundingBox do
+  describe 'initialize' do
+    it 'sets centre to [0,0] by default' do
+      b = BoundingBox.new
+      b.centre.should == [0,0]
+    end
+    it 'sets width to 0 by default' do
+      b = BoundingBox.new
+      b.width.should == 0
+    end
+    it 'sets height to 0 by default' do
+      b = BoundingBox.new
+      b.height.should == 0
+    end
+    it 'sets angle to 0 by default' do
+      b = BoundingBox.new
+      b.angle.should == 0
+    end
+  end
+
+  describe 'centre=' do
+    it 'sets value of centre' do
+      b = BoundingBox.new
+      b.centre = [47,324.928]
+      b.centre.should == [47,324.928]
+    end
+    it "complains when value isn't a hash or array" do
+      b = BoundingBox.new
+      expect { b.centre = 'hello world' }.should(
+        raise_error ArgumentError, "value must be a hash or an array, String given"
+      )
+      expect { b.centre = 25 }.should(
+        raise_error ArgumentError, "value must be a hash or an array, Fixnum given"
+      )
+    end
+  end
 end
