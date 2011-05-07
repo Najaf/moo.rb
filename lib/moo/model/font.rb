7ec840b397bd691f5036f5a38bc651eb828d32e1
Najaf Ali <ali.najaf@gmail.com>
2011-05-07 17:27:19 +0100

    added validation for fonts

--- lib/moo/model/font.rb
+++ lib/moo/model/font.rb
@@ -2,6 +2,41 @@ module Moo
   module Model
     class Font
       attr_accessor :family, :bold, :italic
+      ACCEPTED_FAMILIES = [
+        'arial',
+        'avantgarde',
+        'bryant',
+        'courier',
+        'frutigerltpro',
+        'georgia',
+        'helv',
+        'helvetica',
+        'index',
+        'meta',
+        'officina',
+        'radio',
+        'router',
+        'vagrounded'
+      ]
+      @@default_family = 'helvetica'
+      def initialize
+        @family = @@default_family
+        @bold = false
+        @italic = false
+      end
+
+      alias_method :bold?,   :bold
+      alias_method :italic?, :italic
+
+      def family=value
+        unless value.is_a? String
+          raise ArgumentError, "value must be a String, #{value.class} given"
+        end
+        unless ACCEPTED_FAMILIES.include? value
+          raise ArgumentError, "font family must be on of accepted list, '#{value}' given"
+        end
+        @family = value
+      end
     end
   end
 end
--- spec/model/font_spec.rb
+++ spec/model/font_spec.rb
@@ -3,4 +3,52 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 include Moo::Model
 
 describe Moo::Model::Font do
+  describe 'initialize' do 
+    it 'sets family to helvetica by default' do
+      f = Font.new
+      f.family.should == 'helvetica'
+    end
+
+    it 'sets bold to false by default' do
+      f = Font.new
+      f.bold.should === false
+    end
+
+    it 'sets italic to false by default' do
+      f = Font.new
+      f.italic.should === false
+    end
+  end
+
+  it 'should respond to bold? to determine boldness' do
+      f = Font.new
+      f.bold?.should === false
+      f.bold = true
+      f.bold?.should === true
+  end
+
+  it 'should respond to italic? to determine italicness' do
+      f = Font.new
+      f.italic?.should === false
+      f.italic = true
+      f.italic?.should === true
+  end
+
+  describe 'family=' do
+    it 'should set the value of family' do
+      f = Font.new
+      f.family = 'arial'
+      f.family.should == 'arial'
+    end
+
+    it "should complain if value isn't a string" do
+      f = Font.new
+      expect { f.family = 42 }.should raise_error ArgumentError
+    end
+
+    it "should complain if value isn't in accepted fonts" do
+      f = Font.new
+      expect { f.family = 'This spec is baller' }.should raise_error ArgumentError
+    end
+  end
 end
