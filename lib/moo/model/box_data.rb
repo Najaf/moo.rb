3dfcd2d456a508090747491389e2d41c75f36f1a
Najaf Ali <ali.najaf@gmail.com>
2011-05-07 18:44:23 +0100

    added validations for box data, really need a figure out a way to DRY this up a little

--- lib/moo/model/box_data.rb
+++ lib/moo/model/box_data.rb
@@ -1,6 +1,14 @@
 module Moo
   module Model
     class BoxData < Data
+      attr_reader :colour
+
+      def colour=(value)
+        unless value.kind_of? Colour
+          raise ArgumentError, "expected Colour, got '#{value.class}'"
+        end
+        @colour = value
+      end
     end
   end
 end
--- spec/model/box_data_spec.rb
+++ spec/model/box_data_spec.rb
@@ -8,11 +8,31 @@ describe Moo::Model::BoxData do
       d = BoxData.new
       d.link_id.should == nil
     end
+
+    it 'sets colour to nil' do
+      d = BoxData.new
+      d.colour.should == nil
+    end
   end
+
   describe 'type' do
     it "returns 'boxData'" do
       d = BoxData.new
       d.type.should == 'boxData'
+    end
+  end
+
+  describe 'colour=' do
+    it 'sets the value of colour' do
+      d = BoxData.new
+      c = Colour.new
+      d.colour = c
+      d.colour.should == c
+    end
+
+    it "complains if value isn't a Colour" do
+      d = BoxData.new
+      expect { d.colour = 'woof woof' }.should raise_error ArgumentError
     end
   end
 end
