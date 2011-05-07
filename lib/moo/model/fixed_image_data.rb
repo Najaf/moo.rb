72356d1a175fde0af8aa9ab7988fe4049e09ab3d
Najaf Ali <ali.najaf@gmail.com>
2011-05-07 18:59:33 +0100

    added stuff for fixed image data

--- lib/moo/model/fixed_image_data.rb
+++ lib/moo/model/fixed_image_data.rb
@@ -1,6 +1,14 @@
 module Moo
   module Model
     class FixedImageData < Data
+      attr_reader :resource_uri
+    end
+
+    def resource_uri=value
+      unless value.is_a? String
+        raise ArgumentError, "expected String, got a #{value.class}"
+      end
+      @resource_uri = value 
     end
   end
 end
--- spec/model/fixed_image_data_spec.rb
+++ spec/model/fixed_image_data_spec.rb
@@ -4,15 +4,35 @@ include Moo::Model
 
 describe Moo::Model::FixedImageData do
   describe 'initialize' do
+
     it 'sets link_id to nil' do
       d = FixedImageData.new
       d.link_id.should == nil
     end
+
+    it 'sets resource_uri to nil' do
+      d = FixedImageData.new
+      d.resource_uri.should == nil
+    end
   end
+
   describe 'type' do
     it "returns 'fixedImageData'" do
       d = FixedImageData.new
       d.type.should == 'fixedImageData'
+    end
+  end
+
+  describe 'resource_uri=' do
+    it "sets the value of resource_uri" do
+      d = FixedImageData.new
+      d.resource_uri = 'http://example.com'
+      d.resource_uri.should == 'http://example.com'
+    end
+
+    it "should complain if resource uri is not a string" do
+      d = FixedImageData.new
+      expect { d.resource_uri = 12 }.should raise_error ArgumentError
     end
   end
 end
