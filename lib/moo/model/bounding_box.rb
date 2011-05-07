6315021ca82117fe99ead284ea95013e87ce203b
Najaf Ali <ali.najaf@gmail.com>
2011-05-07 17:07:38 +0100

    added basic input validation to bounding box object

--- lib/moo/model/bounding_box.rb
+++ lib/moo/model/bounding_box.rb
@@ -6,13 +6,39 @@ module Moo
       def initialize
         @centre = [0,0]
         @width, @height, @angle = 0, 0, 0
+
       end
 
       def centre=value
         unless value.is_a? Enumerable
           raise ArgumentError, "value must be Enumerable, #{value.class} given"
         end
-        @centre = value
+        if value.is_a? Hash
+          @centre = [value[:x], value[:y]]
+        else
+          @centre = value
+        end
+      end
+
+      def width=value
+        unless value.is_a? Numeric
+          raise ArgumentError, "value must be Numeric, #{value.class} given"
+        end
+        @width = value
+      end
+
+      def height=value
+        unless value.is_a? Numeric
+          raise ArgumentError, "value must be Numeric, #{value.class} given"
+        end
+        @height = value
+      end
+
+      def angle=value
+        unless value.is_a? Numeric
+          raise ArgumentError, "value must be Numeric, #{value.class} given"
+        end
+        @angle = value
       end
     end
   end
--- spec/model/bounding_box_spec.rb
+++ spec/model/bounding_box_spec.rb
@@ -23,11 +23,18 @@ describe Moo::Model::BoundingBox do
   end
 
   describe 'centre=' do
-    it 'sets value of centre' do
+    it 'sets value of centre to an array from an array' do
       b = BoundingBox.new
       b.centre = [47,324.928]
       b.centre.should == [47,324.928]
     end
+
+    it 'sets value of centre to an array from an x/y keyed hash' do
+      b = BoundingBox.new
+      b.centre = { :y => 12, :x => 15.457 }
+      b.centre.should == [15.457, 12]
+    end
+    
     it "complains when value isn't enumerable" do
       b = BoundingBox.new
       expect { b.centre = 'hello world' }.should(
@@ -36,6 +43,45 @@ describe Moo::Model::BoundingBox do
       expect { b.centre = 25 }.should(
         raise_error ArgumentError, "value must be Enumerable, Fixnum given"
       )
+    end
+  end
+
+  describe 'width=' do
+    it 'sets the value of width' do
+      b = BoundingBox.new
+      b.width = 27
+      b.width.should == 27
+    end
+
+    it 'complains if value is non-numeric' do
+      b = BoundingBox.new
+      expect { b.width = 'hello world' }.should raise_error ArgumentError
+    end
+  end
+
+  describe 'height=' do
+    it 'sets the value of height' do
+      b = BoundingBox.new
+      b.height = 27
+      b.height.should == 27
+    end
+
+    it 'complains if value is non-numeric' do
+      b = BoundingBox.new
+      expect { b.height = 'hello world' }.should raise_error ArgumentError
+    end
+  end
+
+  describe 'angle=' do
+    it 'sets the value of angle' do
+      b = BoundingBox.new
+      b.angle = 27
+      b.angle.should == 27
+    end
+
+    it 'complains if value is non-numeric' do
+      b = BoundingBox.new
+      expect { b.angle = 'hello world' }.should raise_error ArgumentError
     end
   end
 end
