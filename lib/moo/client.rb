c6ca39b3782ba351e626270641ba7d5febebfa2c
Eoin Hennessy <eoin.hennessy@gmail.com>
2012-03-17 15:04:12 -0700

    use 2-legged oauth
    this allows us to simplify Moo::Client

--- lib/moo/client.rb
+++ lib/moo/client.rb
@@ -2,59 +2,18 @@ require 'oauth'
 require 'net/http/post/multipart'
 module Moo
   class Client
-    attr_accessor :oauth_consumer, 
-      :oauth_consumer_key, 
-      :oauth_consumer_secret,
-      :oauth_access_token
+    attr_accessor :oauth_consumer, :oauth_access_token
 
-    def self.oauth_consumer_secret=(secret)
-      @@oauth_consumer_secret = secret
-    end
-
-    def self.oauth_consumer_key=(key)
-      @@oauth_consumer_key = key
-    end
-
-    def self.config
-      yield self if block_given?
-    end
-
-    def initialize(options={})
-      @oauth_consumer_secret = options[:oauth_consumer_secret]
-      @oauth_consumer_key = options[:oauth_consumer_key]
-
-      yield self if block_given?
-
-      # set key/secret to default if not set
-      @oauth_consumer_secret ||= @@oauth_consumer_secret
-      @oauth_consumer_key ||= @@oauth_consumer_key
-
-      # error if the oauth secret and key have not been set by now
-      unless @oauth_consumer_secret and @oauth_consumer_key
-        raise "Moo Client requires OAuth consumer key/secret"
-      end
-
+    def initialize(oauth_consumer_key, oauth_consumer_secret)
       # create the consumer
       @oauth_consumer = OAuth::Consumer.new(
-        @oauth_consumer_key,
-        @oauth_consumer_secret, {
-          site: "https://secure.moo.com",
-          request_token_path: "/oauth/request_token.php",
-          access_token_path: "/oauth/access_token.php",
-          authorize_path: "/oauth/authorize.php" 
+        oauth_consumer_key,
+        oauth_consumer_secret, {
+          site: "https://secure.moo.com"
         })
 
       # create the access token
-      if options[:oauth_access_token] and options[:oauth_access_token_secret]
-        @oauth_access_token = OAuth::AccessToken.new(
-          @oauth_consumer,
-          options[:oauth_access_token],
-          options[:oauth_access_token_secret])
-      end
-    end
-
-    def get_request_token(options)
-      @oauth_consumer.get_request_token(oauth_callback: options[:callback])
+      @oauth_access_token = OAuth::AccessToken.new(@oauth_consumer)
     end
 
     def create_pack(pack, tracking_id=nil)
@@ -66,7 +25,7 @@ module Moo
       params[:trackingId] = tracking_id if tracking_id
       call(params)
     end
-    
+
     def update_pack(pack_id, pack)
       call({
         method: "moo.pack.updatePack",
